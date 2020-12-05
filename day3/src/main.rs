b68125368725c46fc5001b7cb4262a081f872404
Jon <jonathan@boldcommerce.com>
2020-12-05 17:21:50 -0600

    Fixes day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -18,14 +18,42 @@ fn main() {
             width = chars.len();
         }
         height += 1;
+        println!("{:?}",chars);
         row_buffer.push(chars);
+       
     }
     println!("Width: {} Height: {}", width,height);
     let mut trees = 0;
-    for i in 0..height {
-        let x = (3 * i) % width;
-        println!("X:{} Y:{}", x, i);
+    for y in 0..height {
+        for x in 0..width {
+            print!("{}",row_buffer[y][x]);
+        }
+        println!();
     }
+    let slopes = vec!((1,1),(3,1),(5,1),(7,1),(1,2));
+    let mut total: u64  = 1;
+    for slope in slopes {
+        trees = 0;
+        for i in 0..height {
+           
+            let x = (slope.0 * i) % width;
+            let y = slope.1 * i ;
+            if y < height {
+                if row_buffer[y][x] == '#' {
+                
+                    trees += 1;
+                } else {
+                
+                    //println!("{},{}",x,i);
+                }
+            }
+        }
+        println!("Hit {} trees", trees);
+        total *= trees;
+
+    }
+    println!("T Total: {}",total);
+
 
     
 }
