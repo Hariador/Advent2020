fdc36a4570fd2f99604193b9f766fb6d500f674a
Jon <jonathan@boldcommerce.com>
2020-12-13 23:42:01 -0600

    Solved day10

--- day10/day10.rs
+++ day10/day10.rs
@@ -9,12 +9,11 @@ use std::collections::HashMap;
 
 fn main() -> Result<(),io::Error> {
     let filename = env::args().nth(1).expect("Enter a filename");
-    let mut adapaters: Vec<usize> = read_data(&filename)?;
+    let mut adapters: Vec<usize> = read_data(&filename)?;
     let mut cache: HashMap<usize,usize> = HashMap::new();
-    adapaters.sort();
-    adapaters.push(adapaters.last().unwrap() + 3);
-    let solutions = solve(&mut adapaters, &mut cache, 0);
-    println!("Solutions: {}", solutions);
+    adapters.sort();
+    println!("Data: {:?}", adapters);
+    println!("Total solutions is {}",solve(&mut adapters));
     Ok(())
 
 }
@@ -33,53 +32,67 @@ fn read_data(filename: &str) -> io::Result<Vec<usize>> {
     Ok(input_data)
 }
 
-fn solve(adapters: &mut Vec<usize>, cache: &mut HashMap<usize, usize>, tail: usize) -> usize {
-    
-    let mut solutions: usize = 0;
-  //  println!("{:?} TAIL {}",adapters, tail);
-    if valid(adapters) {
-        solutions += 1;
-        for i in tail..adapters.len()-2 {
-            let tmp = adapters[i+1];
-           
-            match cache.get(&tmp) {
-                Some(x) =>  {
-                  //  println!("{:?} CACHE HIT FOR {} VALUE {}",adapters, tmp, x);
-                    if adapters[i+2] - adapters[i] <= 3 {
-                       
-                        adapters.remove(i+1);
-                        solutions += solve(adapters, cache, i);
-                      
-                        cache.insert(tmp,solutions);
-                   
-                        adapters.insert(i+1, tmp);
-                        println!("Solving for {} is {}", tmp, solutions);
-                    }
-                },
-                None => {
-                    if adapters[i+2] - adapters[i] <= 3 {
-                       
-                        adapters.remove(i+1);
-                        solutions += solve(adapters, cache, i);
-                      
-                        cache.insert(tmp,solutions);
-                   
-                        adapters.insert(i+1, tmp);
-                        println!("Solving for {} is {}", tmp, solutions);
-                    }
-                }
-            }  
-        }
+fn solve(adapters: &mut Vec<usize>) -> usize {
+    let mut max = adapters.len()-1;
+    let mut i: usize = 1;
+    let mut muli: usize = 1;
+    loop {
+       match find_anchor(&adapters) {
+           Some(i) => {
+               let mut chunk = adapters.split_off(i);
+               
+               let c = count_solutions(&mut chunk,1);
+               println!("For {:?} count {}", chunk,c);
+               muli = c * muli;
+           },
+           None => {
+            let c = count_solutions(adapters,1);
+            println!("For {:?} count {}", adapters,c);
+            muli = c * muli;
+            break;
+            }
+       }
     }
 
-    return solutions;
+    return muli
+}
+
+fn find_anchor(adapters: &Vec<usize>) -> Option<usize> {
+    let mut end = adapters.len()-1;
+    
+    for i in 1..end {
+        let tar = end -i;
+        if adapters[tar] - adapters[tar-1] == 3 {
+            return Some(tar);
+        }
+    }
+    
+    return None
 }
 
 
+fn count_solutions(adapters: &mut Vec<usize>, tail: usize) -> usize {
+   let mut sol: usize = 0;
+   let max: usize = adapters.len()-1;
+   if valid(adapters) {
+       println!("{:?}", adapters);
+       sol += 1;
+       for i in tail..max {
+            let tmp = adapters[i];
+            adapters.remove(i);
+            sol += count_solutions(adapters, i);
+            adapters.insert(i,tmp);
+       }
+   } 
+
+   return sol;
+    
+}
 
 fn valid(adapters: &Vec<usize>) -> bool {
     
-    let mut last: usize = 0;
+    let mut last: usize = adapters[0];
+
     for i in adapters {
         let diff = i - last;
    
